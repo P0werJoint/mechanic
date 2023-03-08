00af3d026139fe9a0370c6680ffafb649d64bef7
Power <60192115+P0werJoint@users.noreply.github.com>
2023-03-08 15:29:28 +0000

    Add files via upload

---
+++ status.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mechanic Shop</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/public.css">
+</head>
+<body>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:600" rel="stylesheet">
+
+    <ul>
+            <li><a href="index.html">Mechanic</a></li>
+            <li><a class="activem">Check your car Status!</a></li>
+            <li><a href="us.html">Find Us</a></li>
+            <li style="float:right"><a href="help.html">FAQ</a></li>
+    </ul>
+
+    <div class="main">
+        
+        <h1 class="centertxt">Status</h1>
+        <h2 class="centertxt white">temp</h2>
+
+    </div>
+
+    <div class="footer">
+        <p>@Mechanic • 2023 • mechanic@mec.com</p>
+    </div>
+
+</body>
+</html>
